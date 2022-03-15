46549551e7706f124e2665af3e470a18b9f0acf2
VinhPhucs <90695593+VinhPhucs@users.noreply.github.com>
2022-03-15 17:49:59 +0700

    upload

--- src/database.js
+++ src/database.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAOxl26zk1R0su75XN6Ig1uoHtG95mCqes",
-    authDomain: "altamedia-4145a.firebaseapp.com",
-    projectId: "altamedia-4145a",
-    storageBucket: "altamedia-4145a.appspot.com",
-    messagingSenderId: "261248466477",
-    appId: "1:261248466477:web:56046c658a0205c1aa42a4",
-    measurementId: "G-G2NZHEWZP2"
-  };
+  apiKey: "AIzaSyBNtI4qC19vOz7Wx9V9f6i1bNI3uZUc_zw",
+  authDomain: "react-media-6d0a1.firebaseapp.com",
+  projectId: "react-media-6d0a1",
+  storageBucket: "react-media-6d0a1.appspot.com",
+  messagingSenderId: "827094896724",
+  appId: "1:827094896724:web:529100b401b53a244f5dc1",
+  measurementId: "G-72SJ62FPQ7"
+};
 
 const firebaseApp = initializeApp(firebaseConfig);
 
